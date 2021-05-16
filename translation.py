1eac10f299587156fab3e976bd4b77f44c8f5fcc
Amal [Devoloper.TG] <65230767+DevDevolopertg@users.noreply.github.com>
2021-05-16 19:46:30 +0530

    Update translation.py

--- translation.py
+++ translation.py
@@ -1,39 +1,42 @@
 class Translation(object):
-    START_TEXT = """Hello,
-This is a Telegram Rename Bot!
+    START_TEXT = """"💞 Hello,
+
+This is a Telegram Rename Bot ver.2!
 
 <b>Please send me any Telegram file and reply to that file to /rename New Name.mkv</b>
 
+ 👉 <b> Do one By One . Otherwise you will get Permenent Ban </b>
+
+  © @Amal_PM
+  
 /help for more details.."""
     RENAME_403_ERR = "Sorry. You are not permitted to rename this file."
     ABS_TEXT = " Please don't be selfish."
-    UPGRADE_TEXT = "<b>👉 Create own Clone Bot.. </b>  /help for Details"
-    DOWNLOAD_START = "trying to download"
-    UPLOAD_START = "trying to upload"
+    UPGRADE_TEXT = "<b>Contact to @Amal_PM </b> "
+    DOWNLOAD_START = "<b>Trying to Download..📥</b>"
+    UPLOAD_START = <b>Trying to Upload..📥📤</b>"
     RCHD_TG_API_LIMIT = "Downloaded in {} seconds.\nDetected File Size: {}\nSorry. But, I cannot upload files greater than 1.5GB due to Telegram API limitations."
     AFTER_SUCCESSFUL_UPLOAD_MSG = "**Thank you for Using Me🤓.**"
     AFTER_SUCCESSFUL_UPLOAD_MSG_WITH_TS = "Downloaded in {} seconds.\nUploaded in {} seconds."
     NOT_AUTH_USER_TEXT = "Please /upgrade your subscription."
     NOT_AUTH_USER_TEXT_FILE_SIZE = "Detected File Size: {}. Free Users can only upload: {}\nPlease /upgrade your subscription.\nIf you think this is a bug, please contact <a href='https://telegram.dog/ThankTelegram'>@SpEcHlDe</a>"
-    SAVED_CUSTOM_THUMB_NAIL = "Custom File thumbnail saved. This image will be used in the File."
+    SAVED_CUSTOM_THUMB_NAIL = "**✅ Custom thumbnail saved. This image will be used in the file for 24Hr.**"
     DEL_ETED_CUSTOM_THUMB_NAIL = "✅ Custom thumbnail cleared succesfully."
     FF_MPEG_DEL_ETED_CUSTOM_MEDIA = "✅ Media cleared succesfully."
-    SAVED_RECVD_DOC_FILE = "Document Downloaded Successfully."
+    SAVED_RECVD_DOC_FILE = "**Document Downloaded Successfully.**"
     CUSTOM_CAPTION_UL_FILE = " "
     NO_CUSTOM_THUMB_NAIL_FOUND = "No Custom ThumbNail found."
     USER_ADDED_TO_DB = "User <a href='tg://user?id={}'>{}</a> added to {} till {}."
     HELP_USER = """Hai am Rename bot..
     
 1. Send me any Telegram File.
-2. Reply to that message to /rename new name.extension.
+
+2. Send custom Thumbnail
+
+3. Reply to that message to /rename new name.extension.
    
-<b>👉 Create own Clone Bot :</b> 👉 <a href="https://youtu.be/yYHrSnuYXpA">Diploy</a>
-
---------
-
-Support Group : @InFoTelGroup
-© @SpEcHlDe @TGBotsZ"""
-    REPLY_TO_DOC_FOR_RENAME_FILE = "Reply to a Telegram media to `/rename New Name.extension` with custom thumbnail support.."
+--------"""
+    REPLY_TO_DOC_FOR_RENAME_FILE = "**Reply to a Telegram media to `/rename New Name.extension` with custom thumbnail support..**"
     ABUSIVE_USERS = "You are not allowed to use this bot. If you think this is a mistake, please check /me to remove this restriction."
     FREE_USER_LIMIT_Q_SZE = """Cannot Process.
 Free users only 1 request per 30 minutes.
